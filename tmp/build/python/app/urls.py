fc4ae5fb8f9972074a01468dd0c942334d92926e
uoooohub <uoooohub@protonmail.com>
2020-12-09 19:06:47 +0900

    first

--- tmp/build/python/app/urls.py
+++ tmp/build/python/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import Create, listfunc
+from . import views
 
 urlpatterns = [
-   path('', Create.as_view(), name='home'),
-   path('list/', listfunc, name='list'),
+   path('', views.index),
 ]
--- tmp/build/python/app/views.py
+++ tmp/build/python/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-from .models import MY_APP_CLASS_NAME
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
