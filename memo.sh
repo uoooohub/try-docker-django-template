d039d33c708081ec69e11af18b593b130a4cf30b
uoooohub <uoooohub@protonmail.com>
2020-12-09 16:46:19 +0900

    first

---
+++ memo.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/uoooohub/try-hello-app1.git . \
+&& bash create_requirements.sh ./python 'Django==3.1.4' 'requests' 'bs4' \
+&& bash init.sh 'web' 'hello_project' 'hello_app' 'Hello'
+
+
+#git clone https://github.com/uoooohub/try-hello-app1.git . \
+#&& bash create_requirements.sh ./python 'Django==3.1.4' 'requests' 'bs4' \
+#&& bash init.sh 'web' 'hello_project' 'hello_app' 'Hello'
---
+++ tmp/build/python/app/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from .models import MY_APP_CLASS_NAME
+
+# Create your views here.
